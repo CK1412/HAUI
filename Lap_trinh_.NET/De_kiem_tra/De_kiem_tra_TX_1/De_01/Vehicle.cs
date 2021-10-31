30f06cd9dad33fa7b831c349842a77e085dadec9
Cool Kid <65481655+CK1412@users.noreply.github.com>
2021-10-31 09:53:56 +0700

    thêm bài kt TX 1

---
+++ Lap_trinh_.NET/De_kiem_tra/De_kiem_tra_TX_1/De_01/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_01
+{
+    class Vehicle
+    {
+        private string hangSX;
+        private string mauSac;
+
+        public string HangSX
+        {
+            get => hangSX;
+            set => hangSX = value;
+        }
+        public string MauSac
+        {
+            get => mauSac;
+            set => mauSac = value;
+        }
+
+        public virtual void nhap()
+        {
+            Console.Write(" hang san xuat: ");
+            hangSX = Console.ReadLine();
+            Console.Write(" mau sac: ");
+            mauSac = Console.ReadLine();
+        }
+    }
+}
