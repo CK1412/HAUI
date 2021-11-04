5e2ae7083547ce0a931cbd7b2d985bde1b100de3
Cool Kid <65481655+CK1412@users.noreply.github.com>
2021-11-04 17:20:08 +0700

    Add files via upload

---
+++ Lap_trinh_.NET/De_kiem_tra/OOP/Bai_kiem_tra_so_1/De_01/Nguoi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_01
+{
+    class Nguoi
+    {
+        private string hoTen;
+        private string diaChi;
+
+        public string Hoten
+        {
+            get => hoTen;
+            set => hoTen = value;
+        }
+        public string DiaChi
+        {
+            get => diaChi;
+            set => diaChi = value;
+        }
+        public virtual void nhap()
+        {
+            Console.Write(" ho ten: ");
+            hoTen = Console.ReadLine();
+            Console.Write(" dia chi: ");
+            diaChi = Console.ReadLine();
+        }
+
+    }
+}
---
+++ Lap_trinh_.NET/De_kiem_tra/OOP/Bai_kiem_tra_so_1/De_01/NhanVien.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_01
+{
+    class NhanVien : Nguoi
+    {
+        private string maNV;
+        private string chucVu;
+        private double luongCoBan;
+        public string MaNV
+        {
+            get => maNV;
+            set => maNV = value;
+        }
+        public string ChucVu
+        {
+            get => chucVu;
+            set => chucVu = value;
+        }
+        public double LuongCoBan
+        {
+            get => luongCoBan;
+            set => luongCoBan = value;
+        }
+
+        public override void nhap()
+        {
+            base.nhap();
+            Console.Write(" ma nhan vien: ");
+            maNV = Console.ReadLine();
+            Console.Write(" chuc vu: ");
+            chucVu = Console.ReadLine();
+            Console.Write(" luong co ban: ");
+            luongCoBan = Convert.ToDouble(Console.ReadLine());
+        }
+        public int heSoChucVu()
+        {
+            int x;
+            switch (chucVu.ToLower())
+            {
+                case "giam doc":
+                    x = 10;
+                    break;
+                case "truong phong":
+                case "pho giam doc":
+                    x = 6;
+                    break;
+                case "pho phong":
+                    x = 4;
+                    break;
+                default:
+                    x = 2;
+                    break;
+            }
+            return x;
+        }
+    }
+}
