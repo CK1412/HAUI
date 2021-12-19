9407c04e0e084732851d71dc7c4b42b41da4c04c
Cool Kid <65481655+CK1412@users.noreply.github.com>
2021-12-19 16:18:29 +0700

    Add files via upload

---
+++ Lap_trinh_.NET/De_thi/Bai_thi_minh_hoa_KTHP/SanPhamDaBan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_thi_minh_hoa_KTHP
+{
+    class SanPhamDaBan
+    {
+        string maSp;
+        string tenSp;
+        string tenLoaiSp;
+        int sLdaBan;
+        int soTienBan;
+        public string MaSp { get => maSp; set => maSp = value; }
+        public string TenSp { get => tenSp; set => tenSp = value; }
+        public string TenLoaiSp { get => tenLoaiSp; set => tenLoaiSp = value; }
+        public int SLdaBan { get => sLdaBan; set => sLdaBan = value; }
+        public int SoTienBan { get => soTienBan; set => soTienBan = value; }
+
+        public SanPhamDaBan() { }
+        public SanPhamDaBan(string maSp, string tenSp, string tenLoaiSp,int? sLdaBan, int? soTienBan)
+        {
+            this.maSp = maSp;
+            this.tenLoaiSp = tenLoaiSp;
+            this.tenSp = tenSp;
+            this.sLdaBan = sLdaBan ?? 0;
+            this.soTienBan = soTienBan ?? 0;
+        }
+    }
+}
---
+++ Lap_trinh_.NET/De_thi/Bai_thi_minh_hoa_KTHP/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Bai_thi_minh_hoa_KTHP.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bai_thi_minh_hoa_KTHP
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        QLBanHangContext db = new QLBanHangContext();
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query = from loaiSp in db.LoaiSps
+                        join sp in db.SanPhams on loaiSp.MaLoai equals sp.MaLoai
+                        join hdct in db.HoaDonChiTiets on sp.MaSp equals hdct.MaSp
+                        select new SanPhamDaBan(sp.MaSp, sp.TenSp, loaiSp.TenLoai, hdct.SoLuongMua, hdct.SoLuongMua * sp.DonGia);
+
+            var data = query.ToList().GroupBy(e => e.MaSp);
+            foreach (var group in data)
+            {
+                SanPhamDaBan spDaBan = new SanPhamDaBan();
+
+                foreach (var x in group)
+                {
+                    spDaBan.MaSp = x.MaSp;
+                    spDaBan.TenSp = x.TenSp;
+                    spDaBan.TenLoaiSp = x.TenLoaiSp;
+                    spDaBan.SLdaBan += x.SLdaBan;
+                    spDaBan.SoTienBan += x.SoTienBan;
+                }
+                dataGrid.Items.Add(spDaBan);
+            }
+        }
+    }
+}
